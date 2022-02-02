faae9272210d769faff9c55582b17cbe36563821
hideaki10 <senshikou1985@gmail.com>
2022-02-02 16:22:28 +0900

    add partice

---
+++ algo/week1.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+var data = [8]int{
+	24, 18, 12, 9, 17, 66, 32, 4,
+}
+
+func main() {
+
+	var index int
+
+	for k, v := range data {
+		if v == 16 {
+			index = k
+			break
+		}
+		index = -1
+	}
+
+	fmt.Println(index)
+}

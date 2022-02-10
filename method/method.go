0faab3e2ea402953674b532b129944497a2d8550
hideaki10 <senshikou1985@gmail.com>
2022-02-10 22:21:22 +0900

    add methods

--- method/method.go
+++ method/method.go
@@ -2,33 +2,110 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"reflect"
 )
 
-type field struct {
-	name string
-}
+// type field struct {
+// 	name string
+// }
 
-func (p *field) print() {
-	fmt.Println(p.name)
+// func (p *field) print() {
+
+// 	fmt.Println(p.name)
+// }
+
+// func main() {
+// 	data1 := []*field{{"one"}, {"two"}, {"three"}}
+// 	for _, v := range data1 {
+
+// 		//go v.print()
+// 		go (*field).print(v)
+// 	}
+
+// 	data2 := []field{{"four"}, {"five"}, {"six"}}
+// 	for _, v := range data2 {
+
+// 		//go v.print()
+// 		fmt.Println(&v)
+// 		go (*field).print(&v)
+// 	}
+
+// 	time.Sleep(3 * time.Second)
+// }
+
+// func main() {
+// 	arr := [2]int{1, 2}
+// 	res := []*int{}
+// 	for _, v := range arr {
+// 		fmt.Println(&v)
+// 		res = append(res, &v)
+// 	}
+// 	//expect: 1 2
+// 	fmt.Println(*res[0], *res[1])
+// }
+
+// type T struct {
+// 	a int
+// }
+
+// func (t T) M1() {
+// 	t.a = 10
+// }
+
+// func (t *T) M2() {
+// 	t.a = 11
+// }
+
+type T struct{}
+
+func (T) M1() {}
+func (T) M2() {}
+
+type S = T
+
+func (*T) M3() {}
+func (*T) M4() {}
+
+func dumpMethodSet(i interface{}) {
+	dynTyp := reflect.TypeOf(i)
+
+	if dynTyp == nil {
+		fmt.Printf("there is no dynamic type")
+		return
+	}
+
+	n := dynTyp.NumMethod()
+	if n == 0 {
+		fmt.Printf("%s's method set is empty", dynTyp.Name())
+		return
+	}
+
+	fmt.Printf("%s's method set:", dynTyp)
+	for j := 0; j < n; j++ {
+		fmt.Println("-", dynTyp.Method(j).Name)
+	}
+	fmt.Printf("\n")
 }
 
 func main() {
-	data1 := []*field{{"one"}, {"two"}, {"three"}}
-	for _, v := range data1 {
-		fmt.Println("data1")
-		fmt.Println(&v)
-		go v.print()
-		//go (*field).print(v)
-	}
+	// var t T
+	// println(t.a)
 
-	data2 := []field{{"four"}, {"five"}, {"six"}}
-	for _, v := range data2 {
-		fmt.Println("data2")
-		fmt.Println(&v)
-		go v.print()
-		//go (*field).print(&v)
-	}
+	// t.M1()
+	// println(t.a)
 
-	time.Sleep(3 * time.Second)
+	// p := &t
+	// p.M2()
+	// println(t.a)
+
+	// var n int
+	// dumpMethodSet(n)
+	// dumpMethodSet(&n)
+
+	//var t T
+
+	var s S
+
+	dumpMethodSet(s)
+	dumpMethodSet(&s)
 }
---
+++ method/methods/methods.go
@@ -0,0 +1,21 @@
+package methods
+
+// type Interface interface {
+// 	M1()
+// 	M2()
+// }
+
+// type T struct {
+// }
+
+// func (t T) M1() {}
+
+// func (t *T) M2() {}
+
+// func Methods() {
+// 	var t T
+// 	var pt *T
+// 	var i Interface
+// 	i = pt
+// 	i = t
+// }
